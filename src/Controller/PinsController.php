e35ac709424d88ed6908eb71ec532cc52984e8e4
Parisgraphe <jbbornstain.work@gmail.com>
2020-08-20 10:39:27 +0200

    Update delete path

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -82,7 +82,7 @@ class PinsController extends AbstractController
 	}
 
 	/**
-	 * @Route("/pins/{id<\d+>}/delete", name="app_pins_delete", methods="DELETE")
+	 * @Route("/pins/{id<\d+>}", name="app_pins_delete", methods="DELETE")
 	 */
 	public function delete(Request $request, Pin $pin): Response
 	{
